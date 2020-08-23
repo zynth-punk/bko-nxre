4ca37787a95e1e1bdcf655328bf6994612719300
brunokoppel <bruno.koppel@gmail.com>
2020-08-23 05:23:57 -0400

    fix: 🐛 navbar looks more like the old bar

--- libs/ui/src/lib/nav-bar/nav-bar.tsx
+++ libs/ui/src/lib/nav-bar/nav-bar.tsx
@@ -1,50 +1,64 @@
 import React from 'react';
-import { Image, Navbar, Nav, Button } from 'react-bootstrap';
+import { Image, Navbar, Nav, Button, Container } from 'react-bootstrap';
 
-//import BK from './../../assets/DSC_0314.JPG';
+// import BK as logo from './../../index.d.ts';
 import GH from './../../assets/GH.png';
 import LI from './../../assets/LinkedInBnW.png';
 import SP from './../../assets/spotifyBnW.png';
 import IG from './../../assets/IG-BnW.png';
 
+import './nav-bar.scss';
+
 function BKNavBar() {
   return (
     <>
       <Navbar
+        className="container"
         bg="light"
         expand="lg"
-        style={{ display: 'flex', textAlign: 'left', alignContent: 'left' }}
+        style={{
+          display: 'flex',
+          textAlign: 'left',
+          alignContent: 'left',
+          alignItems: 'stretch',
+        }}
       >
-        <Navbar.Brand href="/" style={{ alignSelf: 'center' }}>
-          <Button size="sm" variant="light">
-            <h3
+        <Navbar.Brand style={{ margin: '0.2vmin', display: 'flex' }}>
+          <Button href="/" size="sm">
+            <h4
               style={{
                 padding: '0.4vmin',
                 marginLeft: '-1vmin',
                 marginRight: '-1vmin',
                 marginTop: '0.2vmin',
-                marginBottom: '0.2vmin',
+                marginBottom: '0vmin',
                 color: '#000000',
-                fontSize: '5vmin',
-                fontFamily: 'futura',
+                fontSize: '7vmin',
+                fontFamily: 'monospace',
               }}
             >
               bkö
-            </h3>
+            </h4>
           </Button>
         </Navbar.Brand>
         <Nav
           className="mr-auto"
-          style={{ display: 'flex', flexDirection: 'row' }}
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            textAlign: 'left',
+            alignContent: 'left',
+            alignItems: 'stretch',
+          }}
         >
           <Nav.Link
-            style={{ margin: '0.2vmin' }}
+            style={{ margin: '0.2vmin', display: 'flex' }}
             href="https://github.com/BrunoKoppel"
           >
-            <Button size="sm" variant="light">
+            <Button size="sm">
               <Image
                 src={GH}
-                style={{ width: '5vmin' }}
+                style={{ width: '6vmin' }}
                 alt="logo"
                 rounded
                 fluid
@@ -53,13 +67,13 @@ function BKNavBar() {
           </Nav.Link>
 
           <Nav.Link
-            style={{ margin: '0.2vmin' }}
+            style={{ margin: '0.2vmin', display: 'flex' }}
             href="https://www.linkedin.com/in/bruno-koppel"
           >
-            <Button size="sm" variant="light">
+            <Button size="sm">
               <Image
                 src={LI}
-                style={{ width: '5vmin' }}
+                style={{ width: '6vmin' }}
                 alt="logo"
                 rounded
                 fluid
@@ -68,13 +82,13 @@ function BKNavBar() {
           </Nav.Link>
 
           <Nav.Link
-            style={{ margin: '0.2vmin' }}
+            style={{ margin: '0.2vmin', display: 'flex' }}
             href="https://open.spotify.com/artist/5XK2KlEZ95yiEOqE8BcbgE?si=O6LJDkVWTc-bZ6QWzuuuGA"
           >
-            <Button size="sm" variant="light">
+            <Button size="sm">
               <Image
                 src={SP}
-                style={{ width: '5vmin' }}
+                style={{ width: '6vmin' }}
                 alt="logo"
                 rounded
                 fluid
@@ -83,13 +97,13 @@ function BKNavBar() {
           </Nav.Link>
 
           <Nav.Link
-            style={{ margin: '0.2vmin' }}
+            style={{ margin: '0.2vmin', display: 'flex' }}
             href="https://www.instagram.com/bruno_koppel/"
           >
-            <Button size="sm" variant="light">
+            <Button size="sm">
               <Image
                 src={IG}
-                style={{ width: '5vmin' }}
+                style={{ width: '6vmin' }}
                 alt="logo"
                 rounded
                 fluid
@@ -98,11 +112,21 @@ function BKNavBar() {
           </Nav.Link>
 
           <Nav.Link
-            style={{ margin: '0.2vmin', right: '2vmin', position: 'absolute' }}
+            style={{
+              margin: '0.2vmin',
+              right: '2vmin',
+              position: 'absolute',
+            }}
             aria-controls="basic-navbar-nav"
           >
-            <Button size="sm" variant="light">
-              {/* <Image src={BK} style={{width: "5vmin"}} alt="logo" rounded fluid/> */}
+            <Button size="sm">
+              {/* <Image
+                src={BK}
+                style={{ width: '6vmin' }}
+                alt="logo"
+                rounded
+                fluid
+              /> */}
             </Button>
           </Nav.Link>
         </Nav>
